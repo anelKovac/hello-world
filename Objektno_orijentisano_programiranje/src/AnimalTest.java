fc2aefface8ee76cc1b1c5522259bf7ef4f48a9b
anelKovac <79111695+anelKovac@users.noreply.github.com>
2021-02-19 17:13:13 +0100

    Delete Objektno_orijentisano_programiranje directory

--- Objektno_orijentisano_programiranje/src/AnimalTest.java
+++
@@ -1,9 +0,0 @@
-public class AnimalTest {
-    public static void main(String[] args) {
-        Dog dog = new Dog("Max", "Labrador");
-        Dog famousDog = new Dog("Betoven");
-
-        System.out.println(dog);
-        System.out.println(famousDog);
-    }
-}
